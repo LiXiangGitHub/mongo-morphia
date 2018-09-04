2634f471d640bb8bb60f40b48f63a93c3f5533f7
lixiang <1321058199@qq.com>
2018-09-05 00:50:47 +0800

    下载jar包错误出现错误:java.lang.NoSuchMethodError: com.mongodb.connection.DefaultClusterFactory.create 更换为正确jar 包后正常

--- src/main/java/com/lixiang/mongo/MongoCRUDTest.java
+++ src/main/java/com/lixiang/mongo/MongoCRUDTest.java
@@ -1,7 +1,22 @@
 package com.lixiang.mongo;
-
+import com.mongodb.*;
 public class MongoCRUDTest {
     public static void main(String[] args) {
-
+        //创建Mongo数据源连接
+        Mongo mongo = new Mongo("127.0.0.1",27017);
+        //创建mongo数据库
+        DB db = new DB(mongo,"mongo_test");
+        //获取集合(不存在自动创建)
+        DBCollection  dbcollection= db.getCollection("person");
+        //插入文档
+        DBObject dbObject = new BasicDBObject();
+        dbObject.put("name","lixiang");
+        dbObject.put("age",19);
+        dbcollection.insert(dbObject);
+        //查询文档
+        DBCursor dbCursor= dbcollection.find();
+        for (DBObject obj:dbCursor){
+            System.out.printf(obj.toString());
+        }
     }
 }
