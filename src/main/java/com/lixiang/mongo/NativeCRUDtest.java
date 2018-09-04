071824ba0b600262fcfef998b735ef13e12f7f6a
lixiang <1321058199@qq.com>
2018-09-05 01:05:25 +0800

    clientMongo连接数据库方式 进行数据库操作

--- src/main/java/com/lixiang/mongo/NativeCRUDtest.java
+++ src/main/java/com/lixiang/mongo/NativeCRUDtest.java
@@ -1,7 +1,20 @@
 package com.lixiang.mongo;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 public class NativeCRUDtest {
     public static void main(String[] args) {
-
+        MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("lixiang-test");
+        MongoCollection<Document> mongoCollection=  mongoDatabase.getCollection("t_member");
+        Document document = new Document();
+        document.append("name","xiangzi");
+        document.append("age",18);
+        document.append("adrr","shanghai");
+        mongoCollection.insertOne(document);
     }
 }
